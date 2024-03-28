1b1987a6fc1d405f247fce8b643e5dc0a1047b1c
unknown <parkerosemeyer@gmail.com>
2024-03-28 18:29:30 -0500

    added alert to fill in info

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,8 +4,17 @@ const content = document.getElementById('content')
 const submitBtn = document.getElementById('submit')
 
 // saves post information in local storage
-submitBtn.addEventListener('click', function(event){
-
+submitBtn.addEventListener('click', function (event) {
+    if (username.value === '') {
+        event.preventDefault()
+        alert('Complete putting in username!')
+    } else if (title.value === '') {
+        event.preventDefault()
+        alert('Complete putting in title!')
+    }else if (content.value === '') {
+        event.preventDefault()
+    alert('Complete putting in title!')
+    } else {
     const postLog = JSON.parse(localStorage.getItem('postInfo')) || []
 
     let post = {
@@ -15,4 +24,5 @@ submitBtn.addEventListener('click', function(event){
     }
     postLog.push(post)
     localStorage.setItem('postInfo', JSON.stringify(postLog))
+    }   
 })
